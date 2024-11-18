889871a4f12f7f8baccaaf9fee8168b556286ada
Holly Lampert <holly.lampert@dunelm.com>
2024-11-18 14:04:38 +0000

    Day 3 part 1

--- day3.ts
+++ day3.ts
@@ -7,6 +7,7 @@ const input1 = fs.readFileSync('./inputs/day3part1.txt', 'utf-8').split('\n')
 const input2DArray = input1.map((line) => line.split(''))
 let hashTagArray: number[][] = []
 let earthBlocks = 0
+let unremovable: number[][] = []
 
 //Inital finding of blocks
 for (let i = 0; i < input2DArray.length; i++) {
@@ -17,62 +18,50 @@ for (let i = 0; i < input2DArray.length; i++) {
     }
   }
 }
-console.log(hashTagArray)
 
 //While there are elements in hashTagArray continue searching
 let index = 0
-while (hashTagArray) {
-  console.log(hashTagArray[index])
-  console.log(index)
-  console.log('length', hashTagArray.length)
+while (hashTagArray.length > 0) {
+  if (index > hashTagArray.length - 1) {
+    index = 0
+    for (let i = 0; i < unremovable.length; i++) {
+      input2DArray[unremovable[i][0]][unremovable[i][1]] = '.'
+    }
+
+    unremovable = []
+  }
+
+  //Check all directions
   if (
     input2DArray[hashTagArray[index][0] - 1][hashTagArray[index][1]] !== '#'
   ) {
+    unremovable.push([hashTagArray[index][0], hashTagArray[index][1]])
     hashTagArray.splice(index, 1)
-    if (index === hashTagArray.length - 1) {
-      index = 0
-    }
-    console.log('fail1')
-
     continue
   }
   if (
     input2DArray[hashTagArray[index][0]][hashTagArray[index][1] + 1] !== '#'
   ) {
+    unremovable.push([hashTagArray[index][0], hashTagArray[index][1]])
     hashTagArray.splice(index, 1)
-    if (index === hashTagArray.length - 1) {
-      index = 0
-    }
-    console.log('fail2')
-
     continue
   }
   if (
     input2DArray[hashTagArray[index][0] + 1][hashTagArray[index][1]] !== '#'
   ) {
+    unremovable.push([hashTagArray[index][0], hashTagArray[index][1]])
     hashTagArray.splice(index, 1)
-    if (index === hashTagArray.length - 1) {
-      index = 0
-    }
-    console.log('fail3')
-
     continue
   }
   if (
     input2DArray[hashTagArray[index][0]][hashTagArray[index][1] - 1] !== '#'
   ) {
+    unremovable.push([hashTagArray[index][0], hashTagArray[index][1]])
     hashTagArray.splice(index, 1)
-    if (index === hashTagArray.length - 1) {
-      index = 0
-    }
-    console.log('fail4')
-
     continue
   }
-  console.log(hashTagArray[index])
-
+  earthBlocks += 1
   index += 1
-  if (index === hashTagArray.length - 1) {
-    index = 0
-  }
 }
+
+console.log('Part 1:', earthBlocks)
