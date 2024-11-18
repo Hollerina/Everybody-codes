fdce410f3a1487152621c6f452fc9c8c6f247ed6
Holly Lampert <holly.lampert@dunelm.com>
2024-11-18 17:08:58 +0000

    day5 part1

---
+++ day5.ts
@@ -0,0 +1,39 @@
+import * as fs from 'fs'
+
+/*Inputs*/
+const input1 = fs
+  .readFileSync('./inputs/day5part1.txt', 'utf-8')
+  .split('\n')
+  .map((arr) => arr.replace(/ /g, ''))
+
+let colArray: number[][] = []
+
+for (let i = 0; i < input1[0].length; i++) {
+  let temp: number[] = []
+  for (let j = 0; j < input1.length; j++) {
+    temp.push(Number(input1[j][i]))
+  }
+  colArray.push(temp)
+}
+let numberArray = input1.map((input) => input.split(' '))
+
+console.log(colArray)
+for (let index = 0; index < 10; index++) {
+  let startValue: number = 0
+  startValue = colArray[index % 4].shift() as any
+  let temp: number[] = []
+  temp = [
+    ...colArray[(index + 1) % 4].slice(0, startValue - 1),
+    startValue,
+    ...colArray[(index + 1) % 4].slice(startValue - 1),
+  ] as any
+
+  colArray[(index + 1) % 4] = temp
+}
+
+let result: string = ''
+for (let i = 0; i < colArray.length; i++) {
+  result += String(colArray[i][0])
+}
+
+console.log(result)
