2d399ecbf54aaf4b06688b8c12dfc998edb1be65
Holly Lampert <holly.lampert@dunelm.com>
2024-11-18 14:59:34 +0000

    Day 3 part 2 and 3

--- day3.ts
+++ day3.ts
@@ -1,9 +1,10 @@
 import * as fs from 'fs'
 
 /*Inputs*/
-const input1 = fs.readFileSync('./inputs/day3part1.txt', 'utf-8').split('\n')
+const input1 = fs.readFileSync('./inputs/day3part2.txt', 'utf-8').split('\n')
+const input2 = fs.readFileSync('./inputs/day3part3.txt', 'utf-8').split('\n')
 
-/** Part 1 */
+/** Part 1 & 2*/
 const input2DArray = input1.map((line) => line.split(''))
 let hashTagArray: number[][] = []
 let earthBlocks = 0
@@ -64,4 +65,144 @@ while (hashTagArray.length > 0) {
   index += 1
 }
 
+const input2DArrayPart3 = input2.map((line) => line.split(''))
+let hashTagArrayPart3: number[][] = []
+let earthBlocksPart3 = 0
+let unremovablePart3: number[][] = []
+
+//Inital finding of blocks
+for (let i = 0; i < input2DArrayPart3.length; i++) {
+  for (let j = 0; j < input2DArrayPart3[i].length; j++) {
+    if (input2DArrayPart3[i][j] === '#') {
+      earthBlocksPart3 += 1
+      hashTagArrayPart3.push([i, j])
+    }
+  }
+}
+
+let ind = 0
+while (hashTagArrayPart3.length > 0) {
+  if (ind > hashTagArrayPart3.length - 1) {
+    ind = 0
+    for (let i = 0; i < unremovablePart3.length; i++) {
+      input2DArrayPart3[unremovablePart3[i][0]][unremovablePart3[i][1]] = '.'
+    }
+    unremovablePart3 = []
+  }
+
+  if (
+    hashTagArrayPart3[ind][0] === 0 ||
+    hashTagArrayPart3[ind][1] === 0 ||
+    hashTagArrayPart3[ind][0] === input2DArrayPart3.length - 1 ||
+    hashTagArrayPart3[ind][1] === input2DArrayPart3[0].length - 1
+  ) {
+    unremovablePart3.push([
+      hashTagArrayPart3[ind][0],
+      hashTagArrayPart3[ind][1],
+    ])
+    hashTagArrayPart3.splice(ind, 1)
+    continue
+  }
+  if (
+    input2DArrayPart3[hashTagArrayPart3[ind][0] - 1][
+      hashTagArrayPart3[ind][1]
+    ] !== '#'
+  ) {
+    //Check all directions
+    unremovablePart3.push([
+      hashTagArrayPart3[ind][0],
+      hashTagArrayPart3[ind][1],
+    ])
+    hashTagArrayPart3.splice(ind, 1)
+    continue
+  }
+  if (
+    input2DArrayPart3[hashTagArrayPart3[ind][0] - 1][
+      hashTagArrayPart3[ind][1] - 1
+    ] !== '#'
+  ) {
+    unremovablePart3.push([
+      hashTagArrayPart3[ind][0],
+      hashTagArrayPart3[ind][1],
+    ])
+    hashTagArrayPart3.splice(ind, 1)
+    continue
+  }
+  if (
+    input2DArrayPart3[hashTagArrayPart3[ind][0] - 1][
+      hashTagArrayPart3[ind][1] + 1
+    ] !== '#'
+  ) {
+    unremovablePart3.push([
+      hashTagArrayPart3[ind][0],
+      hashTagArrayPart3[ind][1],
+    ])
+    hashTagArrayPart3.splice(ind, 1)
+    continue
+  }
+  if (
+    input2DArrayPart3[hashTagArrayPart3[ind][0] + 1][
+      hashTagArrayPart3[ind][1] - 1
+    ] !== '#'
+  ) {
+    unremovablePart3.push([
+      hashTagArrayPart3[ind][0],
+      hashTagArrayPart3[ind][1],
+    ])
+    hashTagArrayPart3.splice(ind, 1)
+    continue
+  }
+  if (
+    input2DArrayPart3[hashTagArrayPart3[ind][0] + 1][
+      hashTagArrayPart3[ind][1] + 1
+    ] !== '#'
+  ) {
+    unremovablePart3.push([
+      hashTagArrayPart3[ind][0],
+      hashTagArrayPart3[ind][1],
+    ])
+    hashTagArrayPart3.splice(ind, 1)
+    continue
+  }
+  if (
+    input2DArrayPart3[hashTagArrayPart3[ind][0]][
+      hashTagArrayPart3[ind][1] + 1
+    ] !== '#'
+  ) {
+    unremovablePart3.push([
+      hashTagArrayPart3[ind][0],
+      hashTagArrayPart3[ind][1],
+    ])
+    hashTagArrayPart3.splice(ind, 1)
+    continue
+  }
+  if (
+    input2DArrayPart3[hashTagArrayPart3[ind][0] + 1][
+      hashTagArrayPart3[ind][1]
+    ] !== '#'
+  ) {
+    unremovablePart3.push([
+      hashTagArrayPart3[ind][0],
+      hashTagArrayPart3[ind][1],
+    ])
+    hashTagArrayPart3.splice(ind, 1)
+    continue
+  }
+  if (
+    input2DArrayPart3[hashTagArrayPart3[ind][0]][
+      hashTagArrayPart3[ind][1] - 1
+    ] !== '#'
+  ) {
+    unremovablePart3.push([
+      hashTagArrayPart3[ind][0],
+      hashTagArrayPart3[ind][1],
+    ])
+    hashTagArrayPart3.splice(ind, 1)
+    continue
+  }
+  earthBlocksPart3 += 1
+  ind += 1
+}
+
 console.log('Part 1:', earthBlocks)
+console.log('Part 3:', earthBlocksPart3)
